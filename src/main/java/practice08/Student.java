9bd3a1e11b7c8993c49d6df1f11e8b46bd3aede9
huyf <spyssor@gmail.com>
2019-05-15 15:15:51 +0800

    完成practice08并通过所有测试用例

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,29 @@
 package practice08;
 
-public class Student {
+import java.util.Objects;
+
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (this.equals(klass.getLeader())) {
+            return super.introduce() + " I am a Student. I am Leader of Class " + klass.getNumber() + "." ;
+        } else {
+            return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+        }
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,39 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public boolean isEmptyFromClass(){
+        return klass == null;
+    }
+
+    public String introduce() {
+        if (isEmptyFromClass()) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(this.klass)) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+        }
+    }
 }
