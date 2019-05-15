28d0a95c5ae37e79a574062b767f3730ebda7782
huyf <spyssor@gmail.com>
2019-05-15 15:54:25 +0800

    完成practice09并通过所有测试用例

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,35 @@
 package practice09;
 
 public class Klass {
+
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == null || student.getKlass().number != this.number){
+            System.out.println("It is not one of us.");
+        }else{
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().number = this.number;
+    }
 }
