e424fab1a370f48a37e47b669b70989e748a9e58
huyf <spyssor@gmail.com>
2019-05-15 14:31:22 +0800

    完成practice07并通过所有测试用例

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,19 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class "+ klass.getNumber() +".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public boolean isEmptyFromClass(){
+        return klass == null;
+    }
+
+    public String introduce() {
+        if (isEmptyFromClass()) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(this.klass)) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+        }
+    }
 }
