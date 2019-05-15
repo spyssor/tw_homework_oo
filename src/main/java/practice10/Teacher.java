812f9750a425f6c142a2af2a9f89b7ec07a59717
huyf <spyssor@gmail.com>
2019-05-15 16:18:47 +0800

    完成practice10并通过所有测试用例

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,56 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+
+public class Teacher extends Person{
+
+    private List<Klass> classes;
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public boolean isEmptyFromClass(){
+        return classes == null || classes.size() == 0;
+    }
+
+    public String introduce() {
+        if (isEmptyFromClass()) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < classes.size(); i ++) {
+                sb.append( i == classes.size() - 1 ? classes.get(i).getNumber() : classes.get(i).getNumber() + ", ");
+            }
+            return super.introduce() + " I am a Teacher. I teach Class " + sb.toString() + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (classes.contains(student.getKlass())) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+        }
+    }
+
+    public boolean isTeaching(Student student){
+
+        for (Klass klass : classes) {
+            if (klass.isIn(student)){
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
