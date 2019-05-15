226ae74c397d6909f4f94bdd08c509345cdb8a3f
huyf <spyssor@gmail.com>
2019-05-15 12:22:53 +0800

    完成practice06并通过所有测试用例

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age) {
+        this(name, age,-1);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if (klass != -1) {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
 }
